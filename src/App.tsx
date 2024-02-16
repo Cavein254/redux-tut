118218b9c3e4e21c6242741880443135a3bf7745
johnmuthua <caveomuthama@gmail.com>
2024-02-16 09:04:56 +0300

    Add mult by

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import "./App.css";
+import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 
 function App() {
+  const [start, setStart] = React.useState(0);
   const counter = useSelector((state) => state.counter);
   const dispatch = useDispatch();
 
@@ -15,12 +17,28 @@ function App() {
       type: "DESC",
     });
   };
+  const mulyBy = () => {
+    dispatch({
+      type: "MULT",
+      payload: start,
+    });
+  };
   return (
     <div>
       <h1>Counter App</h1>
       <h2>{counter}</h2>
       <button onClick={increment}>Increment</button>
       <button onClick={decrement}>Decrement</button>
+      <div>
+        <input
+          type="text"
+          value={start}
+          onChange={(e) => setStart(e.target.value)}
+        />
+      </div>
+      <div>
+        <button onClick={mulyBy}>Multiply By</button>
+      </div>
     </div>
   );
 }
--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,8 @@ const reducerfn = (state = { counter: 10 }, action) => {
     return { counter: state.counter * 3 };
   } else if (action.type === "DESC") {
     return { counter: state.counter / 3 };
+  } else if (action.type === "MULT") {
+    return { counter: state.counter * action.payload };
   }
   return state;
 };
